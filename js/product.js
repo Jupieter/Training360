d463b5e1684676ec6ba23a696bdd63b151a98fea
Jupieter <peterjung777@gmail.com>
2020-07-16 17:41:30 +0200

    inactive product outloop

--- js/product.js
+++ js/product.js
@@ -1,6 +1,7 @@
 var serverUrl1 = "http://localhost:3000/foodTypes";
 var serverUrl2 = "http://localhost:3000/keys";
 var serverUrl3 = "http://localhost:3000/foods";
+let tableId = ["Food1", "Food2", "Food3", "Food4", "Food5"];
 let keys = [];
 
 function createAnyElement(name, attributes) {
@@ -23,14 +24,10 @@ function getServerData(url) {
         err => console.error(err)
     );
 }
-let keysPromise = getServerData(serverUrl2)
-console.log(keysPromise);
-keysPromise.then(
+getServerData(serverUrl2).then(
     data => craeteAllTableHeader(data),
     err => console.error(err)
-    );
-// console.log(data);
-    
+);
 
 function createTableHeader(keys, tableID) {
     let table = document.querySelector(`#${tableID}`);
@@ -39,25 +36,69 @@ function createTableHeader(keys, tableID) {
         return;
     };
     table.innerHTML = ""
-    
+
     // let tHead = document.querySelector(`thead`)
 
     let tHead = createAnyElement("thead");
+    let tBody = createAnyElement("tbody");
     let tr = createAnyElement("tr");
 
     for (let k = 3; k < keys.length; k++) {
         let th = createAnyElement("th");
-        th.innerHTML = keys[k]+": ";
+        th.innerHTML = keys[k] + ": ";
         // th.setAttribute("style:", "color: #ff0000");
         tr.appendChild(th);
     }
     tHead.appendChild(tr);
     table.appendChild(tHead);
+    table.appendChild(tBody);
 }
 function craeteAllTableHeader(keys) {
-    let tableId = [ "Food1", "Food2", "Food3", "Food4", "Food5" ];
     for (let i = 0; i < tableId.length; i++) {
-        createTableHeader(keys, tableId[i]); 
+        createTableHeader(keys, tableId[i]);
     }
-    
+
 }
+
+// Fill table with server data
+function fillDataTable(data, tableID) {
+    let table = document.querySelector(`#${tableID}`);
+    if (!table) {
+        console.error('Table is not found')
+        return;
+    }
+    let tBody = table.querySelector("tBody"); // console.log(tBody);
+    tBody.innerHTML = "";
+
+    let tr = createAnyElement("tr");
+    let szar = data[0].active;
+    console.log(szar);
+    if (data.active) {
+
+        for (let row of data) {
+            console.log(row);
+            let tr = createAnyElement("tr");
+            for (let k of keys) {
+                let td = createAnyElement("td")
+                let input = createAnyElement("input", {
+                    class: "form-control",
+                    value: row[k],
+                    name: k
+                });
+                if (k == "id") {
+                    input.setAttribute("readonly", true);
+                }
+                td.appendChild(input)
+                tr.appendChild(td);
+            }
+            let btnGroup = createeBtnGroup();   // console.log(tr);
+            tr.appendChild(btnGroup);
+            tBody.appendChild(tr);
+        }
+    }
+
+}
+getServerData(serverUrl3).then(
+    data => fillDataTable(data, "Food1"),
+    err => console.error(err)
+);
