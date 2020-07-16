f6338b0a00ed142b6bffcf7fee1dab9cdbc69332
Jupieter <Jupieter@users.noreply.github.com>
2020-07-16 21:42:52 +0200

    bootsrap in js on table

--- js/product.js
+++ js/product.js
@@ -31,6 +31,7 @@ getServerData(serverUrl3).then(
 
 function createTableHeader(keys, tableID) {
     let table = document.querySelector(`#${tableID}`);
+    let attributes = {}
     if (!table) {
         console.error('Table is not found')
         return;
@@ -41,16 +42,34 @@ function createTableHeader(keys, tableID) {
 
     let tHead = createAnyElement("thead");
     let tBody = createAnyElement("tbody");
-    let tr = createAnyElement("tr");
+    let tr = createAnyElement("tr" );
+    let divR = createAnyElement("div", {class: "row"} );
 
     for (let k = 3; k < keys.length; k++) {
+        switch (k){
+            case 3:
+                attributes = {class: "col-sm-2"};
+                break
+            case 4:
+                attributes = {class: "col-sm-1"};
+                break
+            case 5:
+                attributes = {class: "col-sm-3"};
+                break
+            case 6:
+                attributes = {class: "col-sm-1"};
+                break
+        }
         let th = createAnyElement("th");
         th.innerHTML = keys[k] + ": ";
+        let divH = createAnyElement("div", attributes );
         // th.setAttribute("style:", "color: #ff0000");
-        tr.appendChild(th);
+        divH.appendChild(th);
+        //  tr.appendChild(divH);
+        divR.appendChild(divH);
     }
-    tHead.appendChild(tr);
-    table.appendChild(tHead);
+    // tHead.appendChild(divR);
+    table.appendChild(divR);
     table.appendChild(tBody);
 }
 
@@ -69,6 +88,7 @@ function craeteAllTable(data) {
 // Fill table with server data
 function fillDataTable(data, ) {
     let table = []; let tBody = [];
+    let attributes ={};
     for (let i = 0; i < tableId.length; i++) {
         let tableID = tableId[i];
         console.log(tableID);
@@ -88,8 +108,8 @@ function fillDataTable(data, ) {
     let tr = createAnyElement("tr");
 
     for (let row of data) {
-        console.log("row: ", row);
-        console.log(row.foodType);
+        // console.log("row: ", row);
+        // console.log(row.foodType);
                 
         if (row.active) {
             let bodyIndex = row.foodType
@@ -97,22 +117,41 @@ function fillDataTable(data, ) {
 
             let tr = createAnyElement("tr");
             for (let k = 3; k < keys.length; k++) {
+                switch (k){
+                    case 3:
+                        attributes = {class: "col-sm-2"};
+                        break
+                    case 4:
+                        attributes = {class: "col-sm-1"};
+                        break
+                    case 5:
+                        attributes = {class: "col-sm-3"};
+                        break
+                    case 6:
+                        attributes = {class: "col-sm-1"};
+                        break
+                }
                 let element = row[keys[k]];
                 let td = createAnyElement("td")
-                td.innerHTML = row[keys[k]];
-
+                td.innerHTML = element;
+                
                 if (keys[k] == "foodname") {
                     td.setAttribute("style", "text-size: 18px" );
                 };
                 if (keys[k] == "action" && element != "") {
                     td.setAttribute("class", "badge badge-secondary" );
                 };
+                let divD = createAnyElement("div", attributes)
 
-                tr.appendChild(td);
+                divD.appendChild(td);
+                tr.appendChild(divD);
             }
+            let divR = createAnyElement("div", {class: "row"})
+            divR.appendChild(divD);
+
             // let btnGroup = createeBtnGroup();   // console.log(tr);
             // tr.appendChild(btnGroup);
-            tBody[bodyIndex].appendChild(tr);
+            tBody[bodyIndex].appendChild(divR);
         }
     }
 
